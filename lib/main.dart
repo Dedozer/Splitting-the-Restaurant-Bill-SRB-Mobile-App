5d0be82387e6cd389a836cacaef490b6d2f813fa
Dedozer <doblygin@gmail.com>
2022-09-03 20:45:02 +0300

    release

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,304 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainScreenWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MainScreenWidget extends StatelessWidget {
+  const MainScreenWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromRGBO(117, 180, 225, 10),
+                Color.fromRGBO(141, 175, 197, 250),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          child: const CalcWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class CalcWidget extends StatefulWidget {
+  const CalcWidget({Key? key}) : super(key: key);
+
+  @override
+  _CalcWidgetState createState() => _CalcWidgetState();
+}
+
+class _CalcWidgetState extends State<CalcWidget> {
+  final _model = CalculateWidgetModel();
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: ChangeNotifierProvider(
+          model: _model,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('All fields must be filled in', style: TextStyle(fontSize: 18,),),
+              SizedBox(
+                height: 10,
+              ),
+              BillAmountWidget(),
+              SizedBox(height: 10),
+              TipPercentageWidget(),
+              DropdownButtonWidget(),
+              SizedBox(height: 10),
+              NumberOfPersonsWidget(),
+              SizedBox(height: 10),
+              SummButtonWidget(),
+              SizedBox(height: 10),
+              ResultWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BillAmountWidget extends StatelessWidget {
+  const BillAmountWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Bill amount',
+        filled: true,
+        fillColor: Colors.white.withAlpha(235),
+        border: const OutlineInputBorder(),
+      ),
+      onChanged: (String value) =>
+          ChangeNotifierProvider.read<CalculateWidgetModel>(context)
+              ?.billAmount = value,
+    );
+  }
+}
+
+class TipPercentageWidget extends StatelessWidget {
+  const TipPercentageWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Tip percentage',
+        filled: true,
+        fillColor: Colors.white.withAlpha(235),
+        border: const OutlineInputBorder(),
+      ),
+      onChanged: (String value) =>
+          ChangeNotifierProvider.read<CalculateWidgetModel>(context)
+              ?.tipPercentage = value,
+    );
+  }
+}
+
+class DropdownButtonWidget extends StatefulWidget {
+  const DropdownButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  State<DropdownButtonWidget> createState() => _DropdownButtonWidgetState();
+}
+
+class _DropdownButtonWidgetState extends State<DropdownButtonWidget> {
+  List<String> items = const [
+    '1',
+    '10',
+    '50',
+    '100',
+  ];
+  late String dropdownvalue = items[0];
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 12.0),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Row(
+          children: [
+            const Text('Rounding up to:', style: TextStyle(fontSize: 18,),),
+            const SizedBox(
+              width: 5,
+            ),
+            DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: DropdownButton(
+                //make true to take width of parent widget
+                underline: Container(), //empty line
+                style: const TextStyle(fontSize: 18, color: Colors.black),
+                value: dropdownvalue,
+                items: items.map((String items) {
+                  return DropdownMenuItem(
+                    value: items,
+                    child: Text(items),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  ChangeNotifierProvider.read<CalculateWidgetModel>(context)
+                      ?.rounding = newValue!;
+                  setState(
+                    () {
+                      dropdownvalue = newValue!;
+                    },
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class NumberOfPersonsWidget extends StatelessWidget {
+  const NumberOfPersonsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Number of persons',
+        filled: true,
+        fillColor: Colors.white.withAlpha(235),
+        border: const OutlineInputBorder(),
+      ),
+      onChanged: (String value) =>
+          ChangeNotifierProvider.read<CalculateWidgetModel>(context)
+              ?.numberOfPersons = value,
+    );
+  }
+}
+
+class SummButtonWidget extends StatelessWidget {
+  const SummButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () =>
+          ChangeNotifierProvider.read<CalculateWidgetModel>(context)
+              ?.calculate(),
+      child: const Text('Сalculate'),
+    );
+  }
+}
+
+class ResultWidget extends StatelessWidget {
+  const ResultWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final value =
+        ChangeNotifierProvider.watch<CalculateWidgetModel>(context)?.result ??
+            '';
+    return Text('Result: $value', style: const TextStyle(fontSize: 18,),);
+  }
+}
+
+class CalculateWidgetModel extends ChangeNotifier {
+  double? _billAmount;
+  double? _tipPercentage;
+  int? _numberOfPersons;
+  int? _rounding = 1;
+  double? result;
+
+  set billAmount(String value) =>
+      _billAmount = double.tryParse(value.replaceAll(',', '.'));
+  set tipPercentage(String value) =>
+      _tipPercentage = double.tryParse(value.replaceAll(',', '.'));
+  set numberOfPersons(String value) => _numberOfPersons = int.tryParse(value);
+  set rounding(String value) => _rounding = int.tryParse(value);
+
+  void calculate() {
+    result = 0.0;
+    if (_billAmount != null &&
+        _tipPercentage != null &&
+        _numberOfPersons != null) {
+      if (_rounding == 1) {
+        result =
+            (_billAmount! + (_tipPercentage! / 100 * _billAmount!)).ceil() /
+                _numberOfPersons!;
+      }
+      if (_rounding == 10) {
+        result =
+            ((_billAmount! + (_tipPercentage! / 100 * _billAmount!)) / 10).ceil() * 10;
+        result = result! / _numberOfPersons!;
+      }
+      if (_rounding == 50) {
+        result = ((_billAmount! + (_tipPercentage! / 100 * _billAmount!)) / 50)
+                .ceil() *
+            50 /
+            _numberOfPersons!;
+      }
+      if (_rounding == 100) {
+        result = ((_billAmount! + (_tipPercentage! / 100 * _billAmount!)) / 100)
+                .ceil() *
+            100/
+            _numberOfPersons!;
+      }
+    } else {
+      result = 0;
+    }
+    notifyListeners();
+  }
+}
+
+class ChangeNotifierProvider<T extends ChangeNotifier>
+    extends InheritedNotifier<T> {
+  const ChangeNotifierProvider({
+    Key? key,
+    required T model,
+    required Widget child,
+  }) : super(
+          key: key,
+          notifier: model,
+          child: child,
+        );
+
+  static T? watch<T extends ChangeNotifier>(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<ChangeNotifierProvider<T>>()
+        ?.notifier;
+  }
+
+  static T? read<T extends ChangeNotifier>(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<ChangeNotifierProvider<T>>()
+        ?.widget;
+    if (widget is ChangeNotifierProvider<T>) {
+      return widget.notifier;
+    } else {
+      return null;
+    }
   }
 }
